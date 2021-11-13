069b7d52106235e2a7019c12e733ed0371ca75e0
Myles Fowler <just_myles@yahoo.com>
2021-11-12 17:42:05 -0800

    Optimizations and clean up

--- parcel_data_load.py
+++ parcel_data_load.py
@@ -1,5 +1,3 @@
-#!/usr/bin/python
-#from itertools import islice
 import gzip
 import os
 from pathlib import Path
@@ -27,22 +25,20 @@ def gather_file_data(param_year):
     with pyodbc.connect(CONNECTION_STRING) as conn:
         cursor = conn.cursor()
         query_check_tbl = "IF EXISTS (SELECT * FROM sysobjects WHERE name = 'ParcelData_{}' AND xtype = 'U') SELECT 1 else SELECT 0".format(param_year)
-        query_check_file = "EXEC property.proc_AssessorFileYearCheck ?;"
+        query_proc_gather_data = "EXEC property.proc_AssessorFileYearCheck ?;"
         cursor.execute(query_check_tbl)
         check_result = cursor.fetchone()
-        for f in check_result:
-            if f[0] ==1:
-                sys.exit('Data already exists. Exiting. Check SELECT TOP 1 * FROM property.ParcelData_{}'.format(param_year))
-            else:
-                cursor.execute(query_check_file,(param_year))
-                result = cursor.fetchall()
-                if result:
-                    for results in result:
-                        data_id = "".join(list(results[0]))
-                        data_filename = "".join(list(results[1]))
-                        return data_id, data_filename, param_year
-                else:
-                    sys.exit("No new data for period {}. Exiting.".format(param_year))
+        if check_result[0] ==1:
+            sys.exit('Data already exists. Exiting. Check SELECT TOP 1 * FROM property.ParcelData_{}'.format(param_year))
+        else:
+            try:
+                cursor.execute(query_proc_gather_data,(param_year))
+                result = cursor.fetchone()
+                data_id = result[0]
+                data_filename = result[1]
+                return data_id, data_filename, param_year
+            except:
+                   sys.exit("No new data for period {}. Exiting.".format(param_year))
 
 def download_data(file_id):
 
@@ -61,7 +57,7 @@ def load_file(filename,year):
         abs_path = os.path.join(os.getcwd(),filename)
         query = "EXEC property.proc_LoadParcelData ?, ?;"
         cursor = conn.cursor()
-        print("Executing SQL.")
+        print(" Executing SQL.")
         cursor.execute(query,(year, abs_path))
         zip_filename = filename + '.gz'
         print("SQL completed. Compressing and moving file to processed directory.")
